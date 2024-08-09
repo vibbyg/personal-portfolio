3510c8cb5cb0071dedf389e464011c257fbbf14f
Vibhor Gautam <vibgofficial@gmail.com>
2024-08-09 13:02:55 -0400

    more mobile ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Holes } from "@/components/Holes";
 export default function Home() {
 
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
+    <main className="w-screen h-screen flex justify-center items-center overflow-y-hidden">
         <h1 className="text-3xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-stone-950 to-violet-900 opacity-75 fixed z-20 pointer-events-none">
           <span>{'hey, i\'m'}</span>
           <a href="https://www.linkedin.com/in/vibhor-gautam/" className="pointer-events-auto hover:bg-gradient-to-l hover:from-cyan-500 hover:to-red-500 hover:bg-clip-text" target="_blank"> vibhor :)</a>
